09c049ee795668d640884b45c5d6db95169eb2d0
VISHWAJEET-07 <90441120+VISHWAJEET-07@users.noreply.github.com>
2023-03-10 16:41:50 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,119 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What value is given for the left margin:-  margin: 5px 10px 3px 8px?",
+    answer: "8px",
+    options: [
+        "8px",
+        "5px",
+        "3px",
+        "10px"
+    ]
+},
+    {
+    numb: 3,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "How do you call the function 'myFunction'?",      
+    answer: "myFunction()",
+    options: [
+        "myFunction",
+        "func myFunction()",
+        "myFunction()",
+        "None of these"
+    ]
+},
+    {
+    numb: 5,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "How can you print information to the console?",
+    answer: "console.log(info)",  
+    options: [
+        "console(info)",
+        "console.log(info)",
+        "print(info)",
+        "print.log(info)"
+    ]
+},
+
+    {
+    numb: 7,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which doctype is correct for HTML5?",
+    answer: "!DOCTYPE html",
+    options: [
+        "!DOCTYPE HTML5",
+        "!DOCTYPE html",
+        "!DOCTYPE",
+        "!Doctype HTML"
+    ]
+},
+
+    {
+    numb: 9,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "What does CSS stands for?",
+    answer: "Cascading Style Sheets",
+    options: [
+        "Common Style Sheets",
+        "Cascading Style Sheets",
+        "Computer Style Sheets",
+        "Colorful Style Sheets"
+    ]
+},
+];
+  
